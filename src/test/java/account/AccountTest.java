8cda15c991db4c5228f8ac4cd7eb665ee8149513
longhoang94 <longsutls@gmail.com>
2018-08-27 19:40:32 +0700

    add test-case testDeposit

--- src/test/java/account/AccountTest.java
+++ src/test/java/account/AccountTest.java
@@ -12,4 +12,12 @@ public class AccountTest {
         double expected = 17500;
         double actual = account.withdraw(2500);
     }
+
+    @Test
+    public void testDeposit() {
+        Account account = new Account(1122, 20000);
+
+        double expected = 23000;
+        double actual = account.deposit(3000);
+    }
 }
