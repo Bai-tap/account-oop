0a02119b1cf972c49a6ca670fab702cd8fee487d
longhoang94 <longsutls@gmail.com>
2018-09-04 19:17:49 +0700

    game ATM

--- src/main/java/account/Application.java
+++ src/main/java/account/Application.java
@@ -1,18 +1,44 @@
 package account;
 
-import java.util.Date;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        Account account = new Account(1122, 20000);
-        account.setAnnualInterestRate(4.5);
-        account.withdraw(2500);
-        System.out.println(account.withdraw(2500));
-        account.deposit(3000);
-        System.out.println(account.deposit(3000));
-        Date now = new Date();
-        account.setDateCreated(now);
-        System.out.println(account.getMonthlyInterest());
-        System.out.println(account.getDateCreated());
+        Account[] accounts = new Account[10];
+        for (int i = 0; i < accounts.length; i++) {
+            accounts[i] = new Account(i, 100);
+        }
+        int choice = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter id: ");
+        int id = scanner.nextInt();
+        do {
+            System.out.println("Main Menu: ");
+            System.out.println("1: Check balance");
+            System.out.println("2: Withdraw");
+            System.out.println("3: Deposit");
+            System.out.println("4: Exit");
+            System.out.println("Enter the choice");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    double balance = accounts[id].getBlance();
+                    System.out.println("The balance is " + balance);
+                    break;
+                case 2:
+                    System.out.print("Enter an amount to withdraw: ");
+                    double withdraw = scanner.nextDouble();
+                    accounts[id].withdraw(withdraw);
+                    break;
+                case 3:
+                    System.out.print("Enter an amount to deposit: ");
+                    double deposit = scanner.nextDouble();
+                    accounts[id].deposit(deposit);
+                    break;
+            }
+        } while (choice != 4);
+
     }
 }
